b1ae5622ed82e420e3dc475a6c80ed4e217d583f
ganeshashet <ganeshashet24@gmail.com>
2022-08-11 17:47:43 +0530

    ganesha 5th push to git

---
+++ 11-8-2022/Library.java
@@ -0,0 +1,45 @@
+/*java program to acheive aggregation, Create class named Book which is having Author class has entity reference
+Class book => bookId,bookName,bookAuthor(Entity reference)
+Author => authorName, authorCountry, authorAge*/
+
+//Author class
+class Author{
+	 String authorName;
+	 String authorCountry;
+	public Author(String authorname,String authorcountry)
+	{
+		authorName=authorname;
+		authorCountry=authorcountry;
+	}
+}
+//Book Class
+class Book{
+	int BookNum;
+	String BookName;
+	Author BookAuthor;//passing class reference to book class
+	
+	public Book(int num,String book, Author atr)
+	{
+	
+		BookNum=num;
+		BookName=book;
+		BookAuthor=atr;
+	}
+	//printing
+	public void bookPrint(){
+		System.out.println("Hello "+BookName+"book id :"+BookNum+" and author is "+BookAuthor.authorName);
+	}
+}
+
+class Libaray{
+	public static void main(String ...args)
+	{
+		//author object
+		Author a1=new Author("Kathy Seirra","UK");
+		//book object
+		Book b1=new Book(1234,"Java SCJP",a1);
+			b1.bookPrint();
+	
+	}
+
+}
